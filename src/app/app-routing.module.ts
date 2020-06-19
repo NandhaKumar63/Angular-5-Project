e9ef7987a430e91293d02d4116e342ba9f77646a
njagadeesan <nandhakumar.jagadeesan@nextsphere.com>
2020-06-19 19:13:24 +0530

    Initial Commit Angular 5 Project

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+// Root level Routes configuration
+const routes: Routes = [
+
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class AppRoutingModule { }
+
